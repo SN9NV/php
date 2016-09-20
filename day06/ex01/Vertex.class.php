34427e3af5ef7a5e8356cda964642a114c5c6304
Angus Dippenaar <angusdippenaar@gmail.com>
2016-09-20 16:07:04 +0200

    ex01

---
+++ day06/ex01/Vertex.class.php
@@ -0,0 +1,83 @@
+<?php
+//	require "Color.class.php";
+
+	class vertex {
+		private $_x = 0;
+		private $_y = 0;
+		private $_z = 0;
+		private $_w = 0;
+	 	private $_color;
+		static $verbose = false;
+
+		public static function doc() {
+			print(file_get_contents('Vertex.doc.txt'));
+		}
+
+		public function __construct(array $args) {
+			$this->_x = $args['x'];
+			$this->_y = $args['y'];
+			$this->_z = $args['z'];
+			if (array_key_exists('w', $args)) {
+				$this->_w = $args['w'];
+			}
+			else {
+				$this->_w = 1.0;
+			}
+			if (array_key_exists('color', $args)) {
+				$this->_color = $args['color'];
+			}
+			else {
+				$this->_color = new Color(array('rgb' => 0xFFFFFF));
+			}
+			if ($this::verbose) {
+				printf("Vertex( x: %.2f, y: %.2f, z: %.2f, %s ) constructed\n", $_x, $_y, $_z, $color);
+			}
+		}
+
+		public function __get($name) {
+			switch ($name) {
+				case 'x':
+					return $this->_x;
+				case 'y':
+					return $this->_y;
+				case 'z':
+					return $this->_z;
+				case 'color':
+					return $this->_color;
+				default:
+					echo "No value with name '$name' exists\n";
+					return NULL;
+			}
+		}
+
+		public function __set($name, $value) {
+			switch ($name) {
+				case 'x':
+					$this->_x = $value;
+				case 'y':
+					$this->_y = $value;
+				case 'z':
+					$this->_z = $value;
+				case 'color':
+					$this->_color = $value;
+				default:
+					echo "No value with name '$name' exists\n";
+			}
+		}
+
+		public function __toString() {
+			if ($this::verbose) {
+				printf("Vertex( x: %.2f, y: %.2f, z: %.2f, %s )", $_x, $_y, $_z, $color);
+			}
+			else {
+				printf("Vertex( x: %.2f, y: %.2f, z: %.2f )", $_x, $_y, $_z);
+			}
+		}
+
+		public function __destruct() {
+			if ($this::verbose) {
+				printf("Vertex( x: %.2f, y: %.2f, z: %.2f, %s ) destructed\n", $_x, $_y, $_z, $color);
+			}
+		}
+	}
+?>
